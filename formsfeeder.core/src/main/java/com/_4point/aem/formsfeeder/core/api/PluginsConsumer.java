2c285c2f611c8044a54bca436b0d1fb9af9e6322
Rob McDougall <rmcdougawww@rogers.com>
2020-05-19 13:47:39 -0400

    Added some comments.

--- formsfeeder.core/src/main/java/com/_4point/aem/formsfeeder/core/api/PluginsConsumer.java
+++ formsfeeder.core/src/main/java/com/_4point/aem/formsfeeder/core/api/PluginsConsumer.java
@@ -2,6 +2,11 @@ package com._4point.aem.formsfeeder.core.api;
 
 import java.util.List;
 
+/**
+ * Implementing this interface in a plug-in allows that plug-in other plugins.
+ *
+ */
+@FunctionalInterface
 public interface PluginsConsumer {
 
 	public void accept(List<NamedFeedConsumer> consumers); 
