0ccabefbdc1cc5874f2bf5414f411f1a03c64e60
Rob McDougall <rmcdougawww@rogers.com>
2020-06-02 18:52:24 -0400

    First command line test case added.

--- formsfeeder.client-cli/src/main/java/formsfeeder/client/cli/CommandLineClient.java
+++ formsfeeder.client-cli/src/main/java/formsfeeder/client/cli/CommandLineClient.java
@@ -1,18 +1,39 @@
 package formsfeeder.client.cli;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
 import org.apache.commons.cli.ParseException;
 
+import com._4point.aem.formsfeeder.core.datasource.DataSource;
 import com._4point.aem.formsfeeder.core.datasource.DataSourceList;
+import com._4point.aem.formsfeeder.core.datasource.DataSourceList.Builder;
 
 import formsfeeder.client.FormsFeederClient;
 import formsfeeder.client.FormsFeederClient.FormsFeederClientException;
 import formsfeeder.client.cli.parameters.AppParameters;
 import formsfeeder.client.cli.parameters.CommandLineAppParameters;
+import formsfeeder.client.cli.parameters.DataSourceInfo;
 import formsfeeder.client.cli.parameters.HostParameters;
 
 public class CommandLineClient {
+	private static Supplier<FileSystem> fsSupplier = FileSystems::getDefault; 
 
 	public static void main(String[] args) {
+		mainline(args, System.in, System.out, System.err);
+	}
+	
+	public static void mainline(String[] args, InputStream in, PrintStream out, PrintStream err) {
 		try {
 			AppParameters cliParameters = CommandLineAppParameters.parseArgs(args);
 
@@ -24,15 +45,65 @@ public class CommandLineClient {
 					  .plugin("Debug")
 					  .build();	
 
-			// TODO: Build DataSourceList
-			
-			DataSourceList result = ffClient.accept(null);
+			DataSourceList result = ffClient.accept(asDataSourceList(cliParameters.dataSourceInfos()));
 			
 			// TODO:  Write out results.
+//			result.list().forEach((ds)->out.println("Found DataSource '" + ds.name() + "'."));
 			
-		} catch (FormsFeederClientException | ParseException e) {
+			FileSystem destFileSystem = fsSupplier.get();
+			Optional<Path> output = cliParameters.output();
+			int resultSize = result.list().size();
+			if (output.isEmpty()) {
+				// No output was specified.
+				if (resultSize == 1) {
+					// Only one output so send to stdout.
+					DataSource dataSource = result.list().get(0);
+					dataSource.inputStream().transferTo(out);
+				} else {
+					// TODO: Figure out what to do if we have multiple. 
+				}
+			} else {
+				// a -o parameter was supplied
+				Path outputPath = destFileSystem.getPath(output.get().toString());
+				if (resultSize == 1) {
+					// Only one output so send we want to send it to a file.
+					if (Files.exists(outputPath)) {
+						if (Files.isDirectory(outputPath) || !Files.isWritable(outputPath)) {
+							throw new FileNotFoundException("Unable to write to file. (" + outputPath.toString() + ").");
+						}
+					}
+					DataSource dataSource = result.list().get(0);
+					try (OutputStream os = Files.newOutputStream(outputPath)) {
+						dataSource.inputStream().transferTo(os);
+					}
+				} else {
+					// Multiple outputs so we want to send to a directory.
+					// TODO: Fill this in.
+				}
+				
+			}
+			
+		} catch (FormsFeederClientException | ParseException | IllegalStateException | IOException e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
+	private static DataSourceList asDataSourceList(List<DataSourceInfo> list) {
+		Builder dslBuilder = DataSourceList.builder();
+		for(var dsInfo : list) {
+			switch(dsInfo.type()) {
+			case PATH:
+				dslBuilder.add(dsInfo.name(), dsInfo.path());
+				break;
+			case STRING:
+				dslBuilder.add(dsInfo.name(), dsInfo.value());
+				break;
+			default:
+				throw new IllegalStateException("Internal error: Found DataSourceInfo object of unknown type (" + dsInfo.type().toString() + ").");
+			}
+		}
+		return dslBuilder.build();
+	}
+
+	
 }
--- formsfeeder.client-cli/src/test/java/formsfeeder/client/cli/CommandLineClientTest.java
+++ formsfeeder.client-cli/src/test/java/formsfeeder/client/cli/CommandLineClientTest.java
@@ -2,7 +2,15 @@ package formsfeeder.client.cli;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
+
+import com._4point.aem.formsfeeder.core.datasource.StandardMimeTypes;
 
 class CommandLineClientTest {
 
@@ -15,8 +23,23 @@ class CommandLineClientTest {
 	//  - Many output DSes - Should write out to directory
 	//  - Many output DSes with selected name - Should write out to a file or stdout.
 	@Test
-	void testMain_OneDS() {
-		String[] args = { "-h", "http://localhost:8080/", "-d", "Param1=Param1Value" };
+	void testMain_OneDS_NoOutput() {
+		String expectedParamValue = "Param1Value";
+		String expectedParamName = "Param1";
+		String[] args = { "-h", "http://localhost:8080/", "-d", expectedParamName + "=" + expectedParamValue };
+		
+		
+		var stdin = new ByteArrayInputStream(new byte[0]);
+		var stdout = new ByteArrayOutputStream();
+		var stderr = new ByteArrayOutputStream();
+		CommandLineClient.mainline(args, stdin, new PrintStream(stdout), new PrintStream(stderr));
+		
+		String stdoutStr = new String(stdout.toByteArray(), StandardCharsets.UTF_8);
+		assertAll(
+				()->assertTrue(stdoutStr.contains(expectedParamName), "Expected '" + stdoutStr + "' to contain '" + expectedParamName + "', but didn't."),
+				()->assertTrue(stdoutStr.contains(expectedParamValue), "Expected '" + stdoutStr + "' to contain '" + expectedParamValue + "', but didn't."),
+				()->assertTrue(stdoutStr.contains(StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString()), "Expected '" + stdoutStr + "' to contain '" + StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString() + "', but didn't.")
+				);	
 	}
 
 }
