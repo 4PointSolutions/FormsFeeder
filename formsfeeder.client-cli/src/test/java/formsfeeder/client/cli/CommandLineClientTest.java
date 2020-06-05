2b1f771ebb1d9d74f59bbec76d0dead63a7085af
Rob McDougall <rmcdougawww@rogers.com>
2020-06-05 14:59:38 -0400

    Added WireMock to the project so that it can be built without a FormsFeeder running.

--- formsfeeder.client-cli/src/test/java/formsfeeder/client/cli/CommandLineClientTest.java
+++ formsfeeder.client-cli/src/test/java/formsfeeder/client/cli/CommandLineClientTest.java
@@ -15,39 +15,97 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import com._4point.aem.formsfeeder.core.datasource.StandardMimeTypes;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import com.github.tomakehurst.wiremock.http.ResponseDefinition;
+import com.github.tomakehurst.wiremock.recording.SnapshotRecordResult;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 import com.jcabi.xml.XML;
 import com.jcabi.xml.XMLDocument;
 
 class CommandLineClientTest {
-	private static final String FORMSFEED_SERVER_LOCATION = "http://localhost:8080/";
+	private static final String FF_SERVER_MACHINE_NAME = "localhost";
+	private static final int FF_SERVER_MACHINE_PORT = 8080;
+
+	private static final String FORMSFEEDER_SERVER_USERNAME = "username";
+	private static final String FORMSFEEDER_SERVER_PASSWORD = "password";
+
 	private static final Path RESOURCES_FOLDER = Paths.get("src", "test", "resources");
 	private static final Path SAMPLE_FILES_DIR = RESOURCES_FOLDER.resolve("SampleFiles");
 	private static final Path SAMPLE_PDF = SAMPLE_FILES_DIR.resolve("SampleForm.pdf");
 
-	// Need to test the following scenarios:
-	//  - No input DSes - should perform a GET
-	//  - One input DS - should POST with the DS in the body
-	//  - Many input DS - should POST multipart/form-data
-	//  = No output DSes - ?
-	//  - One output DS - Should write out to file or stdout
-	//  - Many output DSes - Should write out to .zip
-	//  - Many output DSes with selected name - Should write out to a file or stdout.
+	private static final boolean USE_WIREMOCK = true;
+	private static final boolean WIREMOCK_RECORDING = false;
+
+	private WireMockServer wireMockServer;
+	private static Integer wiremockPort = null;
+	
+	private String formsfeederServerName;
+	private int formsfeederServerPort;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		if (USE_WIREMOCK) {
+			// Let wiremock choose the port for the first test, but re-use the same port for all subsequent tests.
+			wireMockServer = new WireMockServer(wiremockPort == null ? new WireMockConfiguration().dynamicPort().extensions(new ResponseTemplateTransformer(true)) : new WireMockConfiguration().port(wiremockPort).extensions(new ResponseTemplateTransformer(true)));
+	        wireMockServer.start();
+			if (WIREMOCK_RECORDING) {
+				String baseUrl = "http://" + FF_SERVER_MACHINE_NAME + ":"	+ FF_SERVER_MACHINE_PORT;
+				System.out.println("Wiremock recording of '" + baseUrl + "'.");
+				wireMockServer.startRecording(baseUrl);
+			}
+			if (wiremockPort == null) {	// Save the port for subsequent invocations. 
+				wiremockPort = wireMockServer.port();
+			}
+			formsfeederServerName = "localhost";
+			formsfeederServerPort = wiremockPort;
+		} else {
+			formsfeederServerName = FF_SERVER_MACHINE_NAME;
+			formsfeederServerPort = FF_SERVER_MACHINE_PORT;
+		}
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		if (USE_WIREMOCK) {
+	        if (WIREMOCK_RECORDING) {
+	        	SnapshotRecordResult recordings = wireMockServer.stopRecording();
+	        	List<StubMapping> mappings = recordings.getStubMappings();
+	        	System.out.println("Found " + mappings.size() + " recordings.");
+	        	for (StubMapping mapping : mappings) {
+	        		ResponseDefinition response = mapping.getResponse();
+	        		JsonNode jsonBody = response.getJsonBody();
+	        		System.out.println(jsonBody == null ? "JsonBody is null" : jsonBody.toPrettyString());
+	        	}
+	        }
+	        wireMockServer.stop();
+		}
+	}
+	
+	
 	@Test
 	void testMain_OneDS_OneOutput_Stdout() throws Exception {
-		String expectedParamValue = "Param1Value";
-		String expectedParamName = "Param1";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		
+		String expectedParamValue = "ParamValue";
+		String expectedParamName = "Param";
+		String[] args = { "-h", getFFServerLocation(),
+						  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName + "=" + expectedParamValue,
 						  "-p", "Debug"};
 		
@@ -68,10 +126,11 @@ class CommandLineClientTest {
 
 	@Test
 	void testMain_OneDS_OneOutput_File() throws Exception {
-		String expectedParamValue = "Param1Value";
-		String expectedParamName = "Param1";
+		String expectedParamValue = "ParamValue";
+		String expectedParamName = "Param";
 		String expectedOutputLocation = "testMain_OneDS_OneOutput_File_result.txt";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+						  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName + "=" + expectedParamValue,
 						  "-p", "Debug", 
 						  "-o", expectedOutputLocation};
@@ -104,9 +163,11 @@ class CommandLineClientTest {
 
 	@Test
 	void testMain_ManyDS_ManyOutput_Stdout() throws Exception {
-		String[] expectedParamValues = { "Param1Value", "Param2Value", "Param3Value" }; 
+		String samplePdfFilename = SAMPLE_PDF.toString();
+		String[] expectedParamValues = { "Param1Value", "@" + samplePdfFilename, "Param3Value" }; // Two strings and a document parameter
 		String[] expectedParamNames = { "Param1", "Param2", "Param3" }; 
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+						  "-u", getFFServerCredentials(),
 						  "-d", expectedParamNames[0] + "=" + expectedParamValues[0], 
 						  "-d", expectedParamNames[1] + "=" + expectedParamValues[1], 
 						  "-d", expectedParamNames[2] + "=" + expectedParamValues[2], 
@@ -122,16 +183,24 @@ class CommandLineClientTest {
 		ZipEntry ze;
 		int count = 0;
 		while((ze = zis.getNextEntry()) != null) {
-			System.out.println("Found ZipEntry '" + ze.getName() + "' of size '" + ze.getSize() + "'.");
+//			System.out.println("Found ZipEntry '" + ze.getName() + "' of size '" + ze.getSize() + "'.");
 			
 			String zipStr = new String(zis.readAllBytes(), StandardCharsets.UTF_8);
 			final int myCount = expectedParamValues.length - (count + 1);
 			if (myCount >= 0) {	// Skip this section rather than generating out of bounds exceptions.  We'll catch the problem below.
-				assertAll(
-						()->assertTrue(zipStr.contains(expectedParamNames[myCount]), "Expected '" + zipStr + "' to contain '" + expectedParamNames[myCount] + "', but didn't."),
-						()->assertTrue(zipStr.contains(expectedParamValues[myCount]), "Expected '" + zipStr + "' to contain '" + expectedParamValues[myCount] + "', but didn't."),
-						()->assertTrue(zipStr.contains(StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString()), "Expected '" + zipStr + "' to contain '" + StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString() + "', but didn't.")
-						);
+				if (myCount != 1) {	// String Parameters
+					assertAll(
+							()->assertTrue(zipStr.contains(expectedParamNames[myCount]), "Expected '" + zipStr + "' to contain '" + expectedParamNames[myCount] + "', but didn't."),
+							()->assertTrue(zipStr.contains(expectedParamValues[myCount]), "Expected '" + zipStr + "' to contain '" + expectedParamValues[myCount] + "', but didn't."),
+							()->assertTrue(zipStr.contains(StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString()), "Expected '" + zipStr + "' to contain '" + StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString() + "', but didn't.")
+							);
+				} else {		// File Parameter
+					assertAll(
+							()->assertTrue(zipStr.contains(expectedParamNames[myCount]), "Expected '" + zipStr + "' to contain '" + expectedParamNames[myCount] + "', but didn't."),
+							()->assertTrue(zipStr.contains(SAMPLE_PDF.getFileName().toString()), "Expected '" + zipStr + "' to contain '" + SAMPLE_PDF.getFileName().toString() + "', but didn't."),
+							()->assertTrue(zipStr.contains(StandardMimeTypes.APPLICATION_PDF_STR), "Expected '" + zipStr + "' to contain '" + StandardMimeTypes.APPLICATION_PDF_STR + "', but didn't.")
+							);
+				}
 			}
 			count++;
 		}
@@ -146,8 +215,9 @@ class CommandLineClientTest {
 		String[] expectedParamValues = { "Param1Value", "@" + samplePdfFilename, "Param3Value" }; // Two strings and a document parameter
 		String[] expectedParamNames = { "Param1", "Param2", "Param3" }; 
 		String expectedOutputLocation = "testMain_ManyDS_ManyOutput_File_result.zip";
-	String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
-						  "-d", expectedParamNames[0] + "=" + expectedParamValues[0], 
+		String[] args = { "-h", getFFServerLocation(), 
+			  		  	  "-u", getFFServerCredentials(),
+			  		  	  "-d", expectedParamNames[0] + "=" + expectedParamValues[0], 
 						  "-d", expectedParamNames[1] + "=" + expectedParamValues[1], 
 						  "-d", expectedParamNames[2] + "=" + expectedParamValues[2],
 						  "-o", expectedOutputLocation,
@@ -205,9 +275,9 @@ class CommandLineClientTest {
 
 	@Test
 	void testMain_OneDSNoValue_OneOutput_Stdout() throws Exception {
-		String expectedParamValue = "Param1Value";
-		String expectedParamName = "Param1";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String expectedParamName = "ParamNoValue";
+		String[] args = { "-h", getFFServerLocation(), 
+				  		  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName,			// No value supplied, should result in an empty string in the output.
 						  "-p", "Debug"};
 		
@@ -220,7 +290,7 @@ class CommandLineClientTest {
 		String stdoutStr = new String(stdout.toByteArray(), StandardCharsets.UTF_8);
 		assertAll(
 				()->assertTrue(stdoutStr.contains(expectedParamName), "Expected '" + stdoutStr + "' to contain '" + expectedParamName + "', but didn't."),
-				()->assertTrue(stdoutStr.contains("''"), "Expected '" + stdoutStr + "' to contain '" + expectedParamValue + "', but didn't."),
+				()->assertTrue(stdoutStr.contains("''"), "Expected '" + stdoutStr + "' to contain '', but didn't."),
 				()->assertTrue(stdoutStr.contains(StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString()), "Expected '" + stdoutStr + "' to contain '" + StandardMimeTypes.TEXT_PLAIN_UTF8_TYPE.asString() + "', but didn't."),
 				()->assertEquals(0, getFileCount(fs), "Expected no files to be created.")
 				);
@@ -237,7 +307,8 @@ class CommandLineClientTest {
 	void testMain_XML_File(String providedOutputLocation) throws Exception {
 		String expectedParamName = "scenario";
 		String expectedParamValue = "ReturnXml";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+				  		  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName + "=" + expectedParamValue,
 						  "-p", "Mock"};
 
@@ -275,7 +346,8 @@ class CommandLineClientTest {
 	void testMain_XML_Stdout() throws Exception {
 		String expectedParamName = "scenario";
 		String expectedParamValue = "ReturnXml";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+				  		  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName + "=" + expectedParamValue,
 						  "-o", "---",	// Send to stdout
 						  "-p", "Mock"};
@@ -301,7 +373,8 @@ class CommandLineClientTest {
 		String expectedParamValue = "Param1Value";
 		String expectedParamName = "Param1";
 		String expectedOutputLocation = "testMain_OneOutput_CantWriteException_result.txt";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+				  		  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName + "=" + expectedParamValue,
 						  "-p", "Debug", 
 						  "-o", expectedOutputLocation};
@@ -331,7 +404,8 @@ class CommandLineClientTest {
 		String[] expectedParamValues = { "Param1Value", "@" + samplePdfFilename, "Param3Value" }; // Two strings and a document parameter
 		String[] expectedParamNames = { "Param1", "Param2", "Param3" }; 
 		String expectedOutputLocation = "testMain_ManyOutput_CantWriteException_result.zip";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+				  		  "-u", getFFServerCredentials(),
 						  "-d", expectedParamNames[0] + "=" + expectedParamValues[0], 
 						  "-d", expectedParamNames[1] + "=" + expectedParamValues[1], 
 						  "-d", expectedParamNames[2] + "=" + expectedParamValues[2],
@@ -361,7 +435,8 @@ class CommandLineClientTest {
 		String expectedParamName = "scenario";
 		String expectedParamValue = "BadRequestException";
 		String expectedOutputLocation = "testMain_PluginException_result.txt";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
+						  "-u", getFFServerCredentials(),
 						  "-d", expectedParamName + "=" + expectedParamValue,
 						  "-o", expectedOutputLocation,
 						  "-p", "Mock"};
@@ -390,7 +465,7 @@ class CommandLineClientTest {
 		String expectedParamValue = "BadRequestException";
 		String expectedOutputLocation = "testMain_BadAuthParam_result.txt";
 		String badAuthParam = "foobar";
-		String[] args = { "-h", FORMSFEED_SERVER_LOCATION, 
+		String[] args = { "-h", getFFServerLocation(), 
 				  "-d", expectedParamName + "=" + expectedParamValue,
 				  "-o", expectedOutputLocation,
 				  "-u", badAuthParam,
@@ -432,5 +507,13 @@ class CommandLineClientTest {
 			}
 		}
 		return fileCount;
+	}
+	
+	private String getFFServerLocation() {
+		return "http://" + formsfeederServerName + ":" + formsfeederServerPort;
+	}
+	
+	private String getFFServerCredentials() {
+		return FORMSFEEDER_SERVER_USERNAME + ":" + FORMSFEEDER_SERVER_PASSWORD;
 	}
 }
