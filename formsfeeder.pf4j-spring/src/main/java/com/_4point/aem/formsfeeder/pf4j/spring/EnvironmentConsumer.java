b097c420173a30000617037434244f3124f91e72
Rob McDougall <rmcdougawww@rogers.com>
2020-07-13 11:30:15 -0400

    Changes to Environment constants since the server (in addition to plug-ins) will need access to the AEM parameters.

--- formsfeeder.pf4j-spring/src/main/java/com/_4point/aem/formsfeeder/pf4j/spring/EnvironmentConsumer.java
+++ formsfeeder.pf4j-spring/src/main/java/com/_4point/aem/formsfeeder/pf4j/spring/EnvironmentConsumer.java
@@ -8,9 +8,17 @@ import org.springframework.core.env.Environment;
  */
 @FunctionalInterface
 public interface EnvironmentConsumer {
-	public static final String FORMSFEEDER_PLUGINS_ENV_PARAM_PREFIX = "formsfeeder.plugins.";
-	public static final String AEM_HOST_ENV_PARAM = FORMSFEEDER_PLUGINS_ENV_PARAM_PREFIX + "aemHost";
-	public static final String AEM_PORT_ENV_PARAM = FORMSFEEDER_PLUGINS_ENV_PARAM_PREFIX + "aemPort";
+	public static final String FORMSFEEDER_ENV_PARAM_PREFIX = "formsfeeder.";
+	public static final String FORMSFEEDER_PLUGINS_ENV_PARAM_PREFIX = FORMSFEEDER_ENV_PARAM_PREFIX + "plugins.";
+	public static final String AEM_HOST_ENV_PARAM = FORMSFEEDER_ENV_PARAM_PREFIX + "aemHost";
+	public static final String AEM_PORT_ENV_PARAM = FORMSFEEDER_ENV_PARAM_PREFIX + "aemPort";
+	public static final String AEM_USERNAME_ENV_PARAM = FORMSFEEDER_ENV_PARAM_PREFIX + "aemUsername";
+	public static final String AEM_SECRET_ENV_PARAM = FORMSFEEDER_ENV_PARAM_PREFIX + "aemSecret";
+	public static final String AEM_USE_SSL_ENV_PARAM = FORMSFEEDER_ENV_PARAM_PREFIX + "aemUseSsl";
+	// The following are for backwards compatibility
+	public static final String AEM_USE_SSL_PARAM = AEM_USE_SSL_ENV_PARAM;
+	public static final String AEM_USERNAME_PARAM = AEM_USERNAME_ENV_PARAM;
+	public static final String AEM_SECRET_PARAM = AEM_SECRET_ENV_PARAM;
 	
 	public void accept(Environment environment);
 }
