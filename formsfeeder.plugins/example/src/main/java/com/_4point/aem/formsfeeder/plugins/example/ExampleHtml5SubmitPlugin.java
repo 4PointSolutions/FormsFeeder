406ea410de8fbf6352ea0c77e14b5d7ae975bca8
Rob McDougall <rmcdougawww@rogers.com>
2020-10-21 18:14:23 -0400

    feature: Created example HTML5 Submission plugin that returns a PDF.

---
+++ formsfeeder.plugins/example/src/main/java/com/_4point/aem/formsfeeder/plugins/example/ExampleHtml5SubmitPlugin.java
@@ -0,0 +1,159 @@
+package com._4point.aem.formsfeeder.plugins.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.function.Supplier;
+
+import org.pf4j.Extension;
+import org.pf4j.ExtensionPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com._4point.aem.docservices.rest_services.client.output.RestServicesOutputServiceAdapter;
+import com._4point.aem.fluentforms.api.Document;
+import com._4point.aem.fluentforms.api.DocumentFactory;
+import com._4point.aem.fluentforms.api.PathOrUrl;
+import com._4point.aem.fluentforms.api.output.OutputService;
+import com._4point.aem.fluentforms.api.output.OutputService.OutputServiceException;
+import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
+import com._4point.aem.fluentforms.impl.UsageContext;
+import com._4point.aem.fluentforms.impl.output.OutputServiceImpl;
+import com._4point.aem.fluentforms.impl.output.TraditionalOutputService;
+import com._4point.aem.formsfeeder.core.api.AemConfig;
+import com._4point.aem.formsfeeder.core.api.NamedFeedConsumer;
+import com._4point.aem.formsfeeder.core.datasource.DataSource;
+import com._4point.aem.formsfeeder.core.datasource.DataSourceList;
+import com._4point.aem.formsfeeder.core.datasource.DataSourceList.Deconstructor;
+import com._4point.aem.formsfeeder.core.datasource.MimeType;
+import com._4point.aem.formsfeeder.core.datasource.StandardMimeTypes;
+
+@Extension
+public class ExampleHtml5SubmitPlugin implements NamedFeedConsumer, ExtensionPoint {
+	private static final String PLUGIN_NAME = "Html5Submit";
+
+	@Autowired
+	AemConfig aemConfig;
+
+	private static final Supplier<DocumentFactory> docFactorySupplier = SimpleDocumentFactoryImpl::getFactory; 
+	private OutputService outputService;
+	// Creation of TraditionalxxxxxService is redirected through these lambdas so that we can replace them with mocks during unit testing.
+	private Supplier<TraditionalOutputService> tradOutputServiceSupplier = this::createRestServicesTraditionalOutputService;	// Replaced for unit testing.
+
+	@Override
+	public DataSourceList accept(DataSourceList dataSources) throws FeedConsumerException {
+		InputParameters params = InputParameters.from(dataSources);
+			
+		try {
+			final Document result =  outputService().generatePDFOutput()
+													.setContentRoot(PathOrUrl.from(params.contentRoot()))
+													.executeOn(PathOrUrl.from(params.template()), docFactorySupplier.get().create(params.xmlData()));
+
+			final byte[] inlineData = result.getInlineData();
+			final MimeType mimeType = MimeType.of(result.getContentType());
+			
+			return DataSourceList.build(b->b.add("output", inlineData, mimeType));
+		} catch (OutputServiceException | IOException e) {
+			String msg = e.getMessage();
+			throw new FeedConsumerInternalErrorException("Error occurred while generating PDF (" + (msg != null ? msg : e.getClass().getName()) + ").", e);
+		}
+	}
+
+	@Override
+	public String name() {
+		return PLUGIN_NAME;
+	}
+
+	/**
+	 * Instantiate the AEM Forms OutputService and cache it.
+	 * 
+	 * @return AEM OutputService
+	 */
+	private OutputService outputService() {
+		if (this.outputService == null) {
+			this.outputService = new OutputServiceImpl(tradOutputServiceSupplier().get(), UsageContext.CLIENT_SIDE);
+		}
+		return this.outputService;
+	}
+
+	/**
+	 * Instantiate a rest-services.client RestServicesOutputServiceAdapter.
+	 * 
+	 * This is called via the tradOutputServiceSupplier member, if that member's value has not been overridden.
+	 * 
+	 * @return
+	 */
+	private TraditionalOutputService createRestServicesTraditionalOutputService() {
+		TraditionalOutputService outputAdapter = RestServicesOutputServiceAdapter.builder()
+					.machineName(aemConfig.host())
+					.port(aemConfig.port())
+					.basicAuthentication(aemConfig.username(), aemConfig.secret())
+					.useSsl(false)
+					.build();
+		return outputAdapter;
+	}
+	
+	/**
+	 * Getter for tradOutputServiceSupplier function.
+	 * 
+	 * @return
+	 */
+	private final Supplier<TraditionalOutputService> tradOutputServiceSupplier() {
+		return tradOutputServiceSupplier;
+	}
+
+	/**
+	 * Setter for tradOutputServiceSupplier lambda.
+	 * 
+	 * Used for unit testing to replace the call to REST services with a mock object.
+	 * 
+	 * @return
+	 */
+	/* package */ final void tradOutputServiceSupplier(Supplier<TraditionalOutputService> tradOutputServiceSupplier) {
+		this.tradOutputServiceSupplier = tradOutputServiceSupplier;
+	}
+
+	private static class InputParameters {
+		private static final String FORMSFEEDER_PREFIX = "formsfeeder:";
+		private static final String SUBMITTED_DATA_DS_NAME = FORMSFEEDER_PREFIX + "SubmittedData";
+		private static final String TEMPLATE_DS_NAME = FORMSFEEDER_PREFIX + "Template";
+		private static final String CONTENT_ROOT_DS_NAME = FORMSFEEDER_PREFIX + "ContentRoot";
+
+		private final InputStream xmlData;
+		private final String template;
+		private final String contentRoot;
+
+		private InputParameters(InputStream xmlData, String template, String contentRoot) {
+			super();
+			this.xmlData = xmlData;
+			this.template = template;
+			this.contentRoot = contentRoot;
+		}
+		
+		public static InputParameters from(DataSourceList dsl) throws FeedConsumerBadRequestException {
+			final Deconstructor d11r = dsl.deconstructor();
+			DataSource xmlDataDs = d11r.getDataSourceByName(SUBMITTED_DATA_DS_NAME)
+									   .orElseThrow(()->new FeedConsumerBadRequestException("No data was provided to the plugin."));
+			if (!StandardMimeTypes.APPLICATION_XML_TYPE.equals(xmlDataDs.contentType())) {
+				throw new FeedConsumerBadRequestException("Data provided to the plugin was no in XML format.");
+			}
+			
+			String template = d11r.getStringByName(TEMPLATE_DS_NAME)
+					   			  .orElseThrow(()->new FeedConsumerBadRequestException("No template was provided to the plugin."));
+			String contentRoot  = d11r.getStringByName(CONTENT_ROOT_DS_NAME)
+		   			  				  .orElseThrow(()->new FeedConsumerBadRequestException("No content root was provided to the plugin."));
+			return new InputParameters(xmlDataDs.inputStream(), template, contentRoot);
+		}
+
+		final InputStream xmlData() {
+			return xmlData;
+		}
+
+		final String template() {
+			return template;
+		}
+
+		final String contentRoot() {
+			return contentRoot;
+		}
+	}
+
+}
---
+++ formsfeeder.plugins/example/src/test/java/com/_4point/aem/formsfeeder/plugins/example/ExampleHtml5SubmitPluginTest.java
@@ -0,0 +1,120 @@
+package com._4point.aem.formsfeeder.plugins.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import com._4point.aem.fluentforms.api.Document;
+import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
+import com._4point.aem.fluentforms.testing.output.MockTraditionalOutputService;
+import com._4point.aem.formsfeeder.core.api.AemConfig;
+import com._4point.aem.formsfeeder.core.api.AemConfig.Protocol;
+import com._4point.aem.formsfeeder.core.datasource.DataSource;
+import com._4point.aem.formsfeeder.core.datasource.DataSourceList;
+
+class ExampleHtml5SubmitPluginTest {
+	private static final Path RESOURCES_FOLDER = Paths.get("src", "test", "resources");
+	private static final Path SAMPLE_FILES_DIR = RESOURCES_FOLDER.resolve("SampleFiles");
+	private static final Path ACTUAL_RESULTS_DIR = RESOURCES_FOLDER.resolve("ActualResults");
+	private static final Path SAMPLE_XDP = SAMPLE_FILES_DIR.resolve("SampleForm.xdp");
+	private static final Path SAMPLE_DATA = SAMPLE_FILES_DIR.resolve("SampleForm_data.xml");
+
+	private static final String PLUGIN_NAME = "Html5Submit";
+	private static final String FORMSFEEDER_PREFIX = "formsfeeder:";
+	private static final String SUBMITTED_DATA_DS_NAME = FORMSFEEDER_PREFIX + "SubmittedData";
+	private static final String TEMPLATE_DS_NAME = FORMSFEEDER_PREFIX + "Template";
+	private static final String CONTENT_ROOT_DS_NAME = FORMSFEEDER_PREFIX + "ContentRoot";
+
+	private static final boolean SAVE_RESULTS = false;
+	static {
+		if (SAVE_RESULTS) {
+			try {
+				Files.createDirectories(ACTUAL_RESULTS_DIR);
+			} catch (IOException e) {
+				// eat it, we don't care.
+			}
+		}
+	}
+	private static final boolean USE_AEM = false;
+	
+	ExampleHtml5SubmitPlugin underTest = new ExampleHtml5SubmitPlugin();
+
+	@Test
+	void testAccept_Html5Submit() throws Exception {
+		final byte[] expectedResponse = "Expected Response Data".getBytes();
+		String expectedContentType = "application/pdf";
+		final Document expectedResponseDoc = SimpleDocumentFactoryImpl.getFactory().create(expectedResponse);
+		expectedResponseDoc.setContentType(expectedContentType);
+
+		// Mock the REST Output Client
+		if (!USE_AEM) {
+			underTest.tradOutputServiceSupplier(()->MockTraditionalOutputService.createDocumentMock(expectedResponseDoc));
+		}
+		junitx.util.PrivateAccessor.setField(underTest,"aemConfig", mockConfig("localhost", 4502)); 
+		
+		DataSourceList result = underTest.accept(DataSourceList.build(ExampleHtml5SubmitPluginTest::buildInputs));
+
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+		assertEquals(1, result.list().size());
+		DataSource resultDs = result.list().get(0);
+		byte[] resultBytes = resultDs.inputStream().readAllBytes();
+		if (SAVE_RESULTS && USE_AEM) {
+			try (var os = Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("testAccept_Html5Submit.pdf"))) {
+				os.write(resultBytes);;
+			}
+		}
+		assertEquals(expectedContentType,resultDs.contentType().asString());
+		if (!USE_AEM) {
+			// Since we're not using AEM, we can verify the result bytes.
+			assertArrayEquals(expectedResponse, resultBytes, "Expected the result bytes to match the expected response we gave the mock object.");
+		}		
+	}
+
+	@Test
+	void testName() {
+		assertEquals(PLUGIN_NAME, underTest.name());
+	}
+
+	private static DataSourceList.Builder buildInputs(DataSourceList.Builder builder) {
+		return builder.add(SUBMITTED_DATA_DS_NAME, SAMPLE_DATA)
+					  .add(TEMPLATE_DS_NAME, "SampleForm.xdp")
+					  .add(CONTENT_ROOT_DS_NAME, "crx:///content/dam/formsanddocuments/sample-forms");
+	}
+	
+	private static AemConfig mockConfig(String host, int port) {
+		return new AemConfig() {
+
+			@Override
+			public String host() {
+				return host;
+			}
+
+			@Override
+			public int port() {
+				return port;
+			}
+
+			@Override
+			public String username() {
+				return "admin";
+			}
+
+			@Override
+			public String secret() {
+				return "admin";
+			}
+
+			@Override
+			public Protocol protocol() {
+				return null;
+			}
+		};
+	}
+}
